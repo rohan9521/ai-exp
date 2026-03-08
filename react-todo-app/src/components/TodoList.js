48afad20163a9d0720dfe7ed1aeb164087870721
Raja Rohan Ray <rajaro@Rajas-Mac-mini.local>
2026-03-09 01:37:22 +0530

    initial commit

--- react-todo-app/src/components/TodoList.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'; function TodoList({ todos }) {
-  return (
-    <ul>
-      {todos.map((todo, index) => (
-        <li key={index}>{todo}</li>
-      ))}
-    </ul>
-  );}
-export default TodoList;
